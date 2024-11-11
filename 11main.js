7b08889ca6e69051b25bf058fdfd4ed78408cbd9
fuadmohamednur <dayib716@gmail.com>
2024-11-11 19:39:05 +0300

    Arrays and Objects

---
+++ 11main.js
@@ -0,0 +1,28 @@
+
+let testlibrary = ["English_book", "Math_book", "Arabic_book"];
+
+
+let library = [
+    {
+        title: "English_book",
+        author: "xasan",
+        year:   2000,
+    },
+
+    {
+        title: "Math_book",
+        author: "Farxaan",
+        year:  2005,
+    },
+
+    { 
+       title: "Arabic_book",
+       author: "Yacquub",
+       year: 1999,
+    }
+
+]
+
+console.log(library[1].title);
+
+console.log(library[2].author);
